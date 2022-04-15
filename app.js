e4bf0d533478b94e5347cbc7bdc3bf2b2b59e698
0xgamb1t <0xgamb1t@protonmail.com>
2022-04-15 01:22:20 -0600

    <working on hover/click>

--- app.js
+++ app.js
@@ -11,39 +11,10 @@ const gridSlider = document.querySelector('.slider');
 const click = document.querySelector('.click');
 
 const boxes = document.getElementsByName('.box');
+let changeColorEventType = ('click')
 let gridValue = gridSlider.value
 
-function createGrid(boxNum) {
-    const drawingBoard = document.querySelector('.board');
-    const boxes = document.querySelectorAll('.row')
-    boxes.forEach((div) => div.remove());
 
-    // creates rows and columns based on the 'gridSize' value
-    /*
-    drawingBoard.style.gridTemplateRows = `repeat(${gridSize}, 1fr)`;
-    drawingBoard.style.gridTemplateColumns = `repeat(${gridSize}, 1fr`;
-    */
-
-    let counter = 0
-    
-    for (let i = 0; i < boxNum; i++) {
-        const row = drawingBoard.appendChild(document.createElement('div'));
-        row.className = "row"
-        for (x = 0; x < boxNum; x++) {
-            const column = row.appendChild((document.createElement('div')));
-            column.classList.add('box', `number${counter + 1}`,`column${x + 1}`) 
-            counter += 1
-            // console.log(counter)            
-        
-        }
-    }
-    changeBoxColor();
-    gridToggle();
-    resetToggle();
-}
-
-console.log(gridSlider.value)
-createGrid(gridSlider.value)
 
 //const boxes = document.querySelectorAll(".box")
 
@@ -62,20 +33,22 @@ function addNumberToBoxes() {
 addNumberToBoxes();
 */
 
+
+
 // 2. change color when clicked -> based on inputColor
 function changeBoxColor() {
     const boxes = document.querySelectorAll('.box');
-
     boxes.forEach(box => {
-
-        box.addEventListener('click', () => {
+        box.addEventListener(`${changeColorEventType}`, () => {
             inputColor = document.querySelector('.color_picker').value;
-
             box.style.backgroundColor = inputColor
             console.log("abracadabra")
+            console.log(changeColorEventType);
         })
     })
 }
+
+
 
 // 3. gridToggle option
 function gridToggle(){
@@ -131,10 +104,69 @@ newGrid();
 
 function clickToMouseover() {
     click.addEventListener('click', () =>{
-        click.textContent = " ' hover ' "
-        console.log('clicked me!')
+        if (click.textContent == "' click '") {
+            click.textContent = " ' hover ' "
+            changeColorEventType = 'mouseover'
+            console.log('clicked me!')
+            console.log(changeColorEventType)
+
+        } else {
+            click.textContent = "' click '"
+            changeColorEventType = 'click'
+            console.log('clicked me!2nd')
+            console.log(changeColorEventType)
+        }
+        
     })
     
 }
 
 clickToMouseover()
+
+
+
+// function styleCornerBoxes() {
+//     boxes.forEach(box => {
+//         if (box == .style.borderRadius = '8px'
+//     });
+// }
+
+// styleCornerBoxes()
+
+
+function createGrid(boxNum) {
+    
+    const drawingBoard = document.querySelector('.board');
+    const boxes = document.querySelectorAll('.row')
+    boxes.forEach((div) => div.remove());
+
+    // creates rows and columns based on the 'gridSize' value
+    /*
+    drawingBoard.style.gridTemplateRows = `repeat(${gridSize}, 1fr)`;
+    drawingBoard.style.gridTemplateColumns = `repeat(${gridSize}, 1fr`;
+    */
+
+    let counter = 0
+    
+    for (let i = 0; i < boxNum; i++) {
+        const row = drawingBoard.appendChild(document.createElement('div'));
+        row.className = "row"
+        for (x = 0; x < boxNum; x++) {
+            const column = row.appendChild((document.createElement('div')));
+            column.classList.add('box', `number${counter + 1}`,`column${x + 1}`) 
+            counter += 1
+            // console.log(counter)            
+        
+        }
+    }
+
+    
+    gridToggle();
+    resetToggle();
+    changeBoxColor();
+    console.log(changeColorEventType)
+
+}
+
+console.log(gridSlider.value)
+createGrid(gridSlider.value)
